a577d2c07614ff362382b4d336666c41410be7f8
programmer <2625976518@qq.com>
2024-06-03 22:38:09 +0800

    徐扬，吴寰宇快看

---
+++ src/main/java/com/travel/entity/vo/LoginUserVo.java
@@ -0,0 +1,12 @@
+package com.travel.entity.vo;
+
+import lombok.Data;
+
+/*
+ *@ClassName UserVo
+ *@Author Freie  stellen
+ *@Date 2024/6/3 22:30
+ */
+@Data
+public class LoginUserVo {
+}
---
+++ src/main/java/com/travel/entity/vo/PopularVo.java
@@ -0,0 +1,19 @@
+package com.travel.entity.vo;
+
+import lombok.Data;
+
+/*
+ *@ClassName ScencyVo
+ *@Author Freie  stellen
+ *@Date 2024/6/1 15:50
+ */
+@Data
+public class ScencyVo {
+
+    private String name;
+    private String address;
+    private String discribe;
+    private String image;
+    private int liked;
+
+}
